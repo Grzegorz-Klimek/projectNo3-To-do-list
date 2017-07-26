21ba948195528fd22cc36d7a283e3c7484110ee5
Grzegorz-Klimek <grzegorzkklimek@gmail.com>
2017-07-26 14:14:39 +0200

    complete shopping list app

--- js/main.js
+++ js/main.js
@@ -1,4 +1,31 @@
-$('li').click(function() {
-    $(this).css('color', 'red');
-    $(this).css('text-decoration', 'line-through');
+var $ul = $('ul');
+var $inputAdd = $("input[type='text']");
+
+$ul.on('click', 'li', function () {
+    $(this).toggleClass('product-bought');
+});
+
+$ul.on('click', 'span', function (e) {
+    $(this)
+        .parent()
+        .slideUp(300, function () {
+            $(this).remove();
+        });
+    e.stopPropagation();
+});
+
+$inputAdd
+    .click(function () {
+        $(this).val('');
+    })
+    .keypress(function (e) {
+        if (e.which === 13) {
+            var product = $(this).val();
+            $(this).val('');
+            $('ul').append('<li>' + '<span><i class="fa fa-trash-o" aria-hidden="true"></i></span>' + product + '</li>');
+        }
+    });
+
+$('.fa-plus-circle').click(function() {
+    $inputAdd.fadeToggle();
 });
